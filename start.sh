c713932cb65d598427df8528430d0884695447a7
HDCinema4U <115459136+HDCinema4U@users.noreply.github.com>
2023-02-25 18:45:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kdbotz/Lucifer.git /Lucifer
+  git clone https://github.com/HDCinema4U/King-Repo /King-Repo
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Lucifer
